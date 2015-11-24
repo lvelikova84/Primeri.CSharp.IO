4c2b41a0f2781fc422b3d628555e138cc06900dc
Лилия Великова <lvelikova84@abv.bg>
2015-11-24 08:46:33 +0200

    Модул 08, Видео 02 - Запси на текстов файл

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	public class IOSettings
+	{
+		private STable _stable;
+		public IOSettings (STable stable)
+		{
+			_stable = stable;
+		}
+
+		public bool save ()
+		{
+			try {
+				string _temp = "";
+				_temp = String.Join ("\r\n", _stable.stable);
+
+					//Запис на текстов фаил
+					System.IO.File.WriteAllText ("C:\\aula2\\test.txt", _temp);
+
+				return true;
+			} catch {
+			}
+			return false;
+		}
+	}
+}
+
