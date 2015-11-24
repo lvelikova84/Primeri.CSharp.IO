7cc7ea0ce23d873724ff38e3856cae235c6e0379
Лилия Великова <lvelikova84@abv.bg>
2015-11-24 09:37:32 +0200

    Модул 08, Видео 03 - Четене на текстов файл

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -10,6 +10,12 @@ namespace IOTextFiles
 			_stable = stable;
 		}
 
+		public string getPath ()
+		{
+			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+		
+			return _path;
+		}
 		public bool save ()
 		{
 			try {
@@ -21,6 +27,23 @@ namespace IOTextFiles
 
 				return true;
 			} catch {
+			}
+			return false;
+		}
+		public bool open ()
+		{
+			try
+			{
+					string _temp = System.IO.File.ReadAllText ( getPath () );
+					string[] _table = _temp.Replace("\r","").Split('\n');
+
+					for ( int i = 0; i < _table.Length; i ++)
+					{
+						_stable.stable[i] = _table[i];
+					}
+
+					return true;
+			}catch{
 			}
 			return false;
 		}
